ec5f0a75b3c2762833044309e318791ec52a30fc
Mohammad <mohammadmrd74@gmail.com>
2022-08-24 01:12:13 +0430

    asd

--- annenkovstore/plugins/hotjar.client.js
+++ annenkovstore/plugins/hotjar.client.js
@@ -1,4 +1,4 @@
-(function(h,o,t,j,a,r){
+let a = (function(h,o,t,j,a,r){
     h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
     h._hjSettings={hjid:3120863,hjsv:6};
     a=o.getElementsByTagName('head')[0];
@@ -6,3 +6,5 @@
     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
     a.appendChild(r);
 })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
+export default a;
