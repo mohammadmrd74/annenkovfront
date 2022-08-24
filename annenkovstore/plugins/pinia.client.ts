40ffd369d9962fd424fb115df6d13c0240f73d8c
Mohammad <mohammadmrd74@gmail.com>
2022-08-24 23:09:52 +0430

    add max age

--- annenkovstore/plugins/pinia.client.ts
+++ annenkovstore/plugins/pinia.client.ts
@@ -2,5 +2,9 @@ import { createNuxtPersistedState } from 'pinia-plugin-persistedstate'
 import { defineNuxtPlugin, useCookie } from '#app'
 
 export default defineNuxtPlugin(nuxtApp => {
-  nuxtApp.$pinia.use(createNuxtPersistedState(useCookie))
+  nuxtApp.$pinia.use(createNuxtPersistedState(useCookie, {
+    cookieOptions: {
+      maxAge: 604000
+    }
+  }))
 })
